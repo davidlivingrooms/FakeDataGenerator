91020742b8a7fcc117d6ee7faa1b38a4cb758699
David Salas <dsalas@alteryx.com>
2017-08-10 10:33:42 -0600

    Checking in image directory

--- FakeDataGeneratorEngine.py
+++ FakeDataGeneratorEngine.py
@@ -1,7 +1,6 @@
 import AlteryxPythonSDK
 import xml.etree.ElementTree as ET
 from faker import Factory
-fake = Factory.create()
 
 class AyxPlugin:
     def __init__(self, n_tool_id, alteryx_engine, generic_engine, output_anchor_mgr):
@@ -33,7 +32,9 @@ class AyxPlugin:
         self.output_type = None
 
         self.num_records = 0
-        self.fakeFields = {}
+        self.fake_fields = {}
+        self.locale = ''
+        self.fake = None
 
         return
 
@@ -46,16 +47,15 @@ class AyxPlugin:
     #
     def pi_init(self, str_xml):
         try:
-            self.output_message('xml', AlteryxPythonSDK.EngineMessageType.info, str_xml) # DEBUG
             root = ET.fromstring(str_xml)
             self.num_records = root.find('NumRecords').text
-            self.output_message('num_records', AlteryxPythonSDK.EngineMessageType.info, self.num_records) # DEBUG
-            # self.n_record_count = int(root.find('StartValue').text) - 1
-            fieldName = root.find('columnName').text
-            self.output_message('columnName', AlteryxPythonSDK.EngineMessageType.info, fieldName) # DEBUG
-            fakerProvider = root.find('fakerProvider').text
-            self.output_message('fakerProvider', AlteryxPythonSDK.EngineMessageType.info, fakerProvider) # DEBUG
-            self.fakeFields[fieldName] = fakerProvider
+            fake_fields = root.find('FakeFields')
+            locale = root.find('Locale').text
+            self.output_message('locale', AlteryxPythonSDK.EngineMessageType.info, locale) # DEBUG
+            self.fake = Factory.create(locale)
+            # self.output_message('locale', AlteryxPythonSDK.EngineMessageType.info, self.locale) # DEBUG
+            for field in fake_fields:
+                self.fake_fields[field.find('FieldName').text] = field.find('Provider').text
         except AttributeError:
             self.output_message('pi_init', AlteryxPythonSDK.EngineMessageType.info, 'Invalid XML: ' + str_xml)
             raise
@@ -86,18 +86,86 @@ class AyxPlugin:
     def pi_add_outgoing_connection(self, str_name):
         return True
 
+    def get_generated_value_from_provider(self, provider):
+        # self.output_message('provider', AlteryxPythonSDK.EngineMessageType.info, provider)
+        generated_value = ''
+        if provider == 'address':
+            generated_value = self.fake.address()
+        elif provider == 'country':
+            generated_value = self.fake.country()
+        elif provider == 'name':
+            generated_value = self.fake.name()
+        elif provider == 'phone_number':
+            generated_value = self.fake.phone_number()
+        elif provider == 'mac_address':
+            generated_value = self.fake.mac_address()
+        elif provider == 'user_name':
+            generated_value = self.fake.user_name()
+        elif provider == 'url':
+            generated_value = self.fake.url()
+        elif provider == 'image_url':
+            generated_value = self.fake.image_url()
+        elif provider == 'company':
+            generated_value = self.fake.company()
+        elif provider == 'bs':
+            generated_value = self.fake.bs()
+        elif provider == 'day_of_week':
+            generated_value = self.fake.day_of_week()
+        elif provider == 'free_email':
+            generated_value = self.fake.free_email()
+        elif provider == 'job':
+            generated_value = self.fake.job()
+        elif provider == 'ean8':
+            generated_value = self.fake.ean8()
+        elif provider == 'credit_card_full':
+            generated_value = self.fake.credit_card_full()
+        elif provider == 'paragraph':
+            generated_value = self.fake.paragraph()
+        elif provider == 'text':
+            generated_value = self.fake.text()
+        elif provider == 'binary':
+            generated_value = self.fake.binary()
+        elif provider == 'file_path':
+            generated_value = self.fake.file_path()
+        elif provider == 'sha256':
+            generated_value = self.fake.sha256()
+        elif provider == 'md5':
+            generated_value = self.fake.md5()
+        elif provider == 'password':
+            generated_value = self.fake.password()
+        elif provider == 'license_plate':
+            generated_value = self.fake.license_plate()
+        elif provider == 'isbn13':
+            generated_value = self.fake.isbn13()
+        elif provider == 'latitude':
+            generated_value = self.fake.latitude()
+        elif provider == 'longitude':
+            generated_value = self.fake.longitude()
+        elif provider == 'simple_profile':
+            generated_value = self.fake.simple_profile()
+        elif provider == 'ssn':
+            generated_value = self.fake.ssn()
+        elif provider == 'user_agent':
+            generated_value = self.fake.user_agent()
+        elif provider == 'simple_profile':
+            generated_value = self.fake.simple_profile()
+        elif provider == 'profile':
+            generated_value = self.fake.profile()
+        elif provider == 'ipv4':
+            generated_value = self.fake.ipv4()
+        elif provider == 'ipv6':
+            generated_value = self.fake.ipv6()
+        else:
+            return 'No Provider found'
+
+        return generated_value
     #
     # pi_push_all_records will be called if there are no inputs connected to this tool
     #
     def pi_push_all_records(self, n_record_limit):
         record_info = AlteryxPythonSDK.RecordInfo(self.generic_engine)
-
-        self.output_message('fakeFields', AlteryxPythonSDK.EngineMessageType.info, self.fakeFields) # DEBUG
-        for fieldName, fieldProvider in self.fakeFields.items():
-            self.output_message('fieldName', AlteryxPythonSDK.EngineMessageType.info, fieldName)
-            self.output_message('fieldProvider', AlteryxPythonSDK.EngineMessageType.info, fieldProvider)
+        for fieldName, fieldProvider in self.fake_fields.items():
             # add_field(field_name, type, size, scale, source, description)
-
             record_info.add_field(
                 fieldName,
                 AlteryxPythonSDK.FieldType.v_wstring, # support other data types in the future
@@ -108,14 +176,16 @@ class AyxPlugin:
             )
 
         self.output_anchor.init(record_info, '')
-        self.output_message('fakeFieldsLength', AlteryxPythonSDK.EngineMessageType.info, len(self.fakeFields)) # DEBUG
         self.record_creator = record_info.construct_record_creator()
-        self.output_message('recordFieldsLength', AlteryxPythonSDK.EngineMessageType.info, record_info.num_fields) # DEBUG
+        # self.output_message('recordFieldsLength', AlteryxPythonSDK.EngineMessageType.info, record_info.num_fields) # DEBUG
         for i in range(0, int(self.num_records)):
-            for field_num in range(0, record_info.num_fields):
-                field = record_info[field_num]
-                field.set_from_string(self.record_creator, 'sdf')
-                self.output_message('field description', AlteryxPythonSDK.EngineMessageType.info, field.description)
+            j = 0
+            for field_name, field_provider in self.fake_fields.items():
+                field = record_info[j]
+                field_value = str(self.get_generated_value_from_provider(field_provider)) # TODO eventually generate different types besides string
+                field.set_from_string(self.record_creator, field_value)
+                j = j + 1
+
             out_record = self.record_creator.finalize_record()
             self.output_anchor.push_record(out_record, False)
             self.record_creator.reset(0)
